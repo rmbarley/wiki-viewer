3d495a279efe17ddecac74922667c2fcc51fadbc
Ryan <rmbarley@gmail.com>
2017-06-13 20:05:47 -0400

    Convert http call to https call

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(input).keypress(function(e) {
   if (e.keyCode == 13) {
     var articleList = [];
 
-    wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&search=" + $(input).val();
+    wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&search=" + $(input).val();
     $(wiki).empty();
     $.ajax({
       url: wikiUrl,
@@ -14,7 +14,7 @@ $(input).keypress(function(e) {
       type: 'POST',
       success: function(response) {
         for (var i = 0; i < response[1].length; i++) {
-          $(wiki).append("<li><strong><a href='http://en.wikipedia.org/wiki/" + response[1][i] + "' target='_blank'>" + response[1][i] + "</strong></a><p>" + response[2][i] + "</p></li>");
+          $(wiki).append("<li><strong><a href='https://en.wikipedia.org/wiki/" + response[1][i] + "' target='_blank'>" + response[1][i] + "</strong></a><p>" + response[2][i] + "</p></li>");
         }
         $(wiki).append("<i class='fa fa-times-circle-o fa-4x' onclick='clearSearch()'></i>");
       }
